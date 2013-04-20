8b365f1683cf8c926649ea8f73c88815f831b4c8
Hongmin Wang <whunmr@gmail.com>
2013-04-20 15:51:52 +0800

    refactor

--- ioc-container/src/test/java/com/expressioc/ExpressContainerTest.java
+++ ioc-container/src/test/java/com/expressioc/ExpressContainerTest.java
@@ -99,9 +99,5 @@ public class ExpressContainerTest {
         container.getComponent(IA_As_ConstructorArg.class);
     }
 
-
-
-    //TODO: should inject basic type
     //TODO: how to fix set again after construction.
-    //TODO: init container with package name, and init with annotations in specified package.
 }
